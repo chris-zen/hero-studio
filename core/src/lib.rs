d2e4b3e10594c94940e58ea76a56aafd9650bf74
Roberto Huertas <roberto.huertas@outlook.com>
2018-12-26 22:20:09 +0100

    allow unused imports

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,11 +1,10 @@
+// FIXME Remove when the code gets more stable
+#![allow(unused_imports)]
+#![allow(dead_code)]
 pub mod color;
-
-pub mod midi;
-
 pub mod config;
-
+pub mod midi;
+pub mod song;
 pub mod studio;
-
 pub mod time;
 
-pub mod song;
