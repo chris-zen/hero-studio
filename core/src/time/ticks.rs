c52cb3acf7690612ab2fbf0734e0c8ae29774922
engid <ngideo@gmail.com>
2019-04-21 20:16:58 -0500

    increasing resolution for ticks

--- core/src/time/ticks.rs
+++ core/src/time/ticks.rs
@@ -5,7 +5,7 @@ use std::{
 
 use crate::time::{clock, ClockTime, Signature, Tempo};
 
-pub const TICKS_RESOLUTION: u64 = 2 * 2 * 3 * 3 * 5 * 5 * 7 * 11; //34650
+pub const TICKS_RESOLUTION: u64 = 508032000; // 2^10 * 3^4 * 5^3 * 7^2
 
 #[derive(Debug, Eq, Copy, Clone)]
 pub struct TicksTime(u64);
@@ -132,7 +132,7 @@ mod test {
     let signature = Signature::new(4, 4);
     let tempo = Tempo::new(120);
     let ticks = TicksTime::per_minute(signature, tempo);
-    assert_eq!(ticks.0, 33_264_000);
+    assert_eq!(ticks.0, 243_855_360_000);
   }
 
   #[test]
