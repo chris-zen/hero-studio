6462e4b39b3a22a620cfb537c2e7cde0f4726d12
engid <ngideo@gmail.com>
2019-04-21 20:16:58 -0500

    increasing resolution for ticks

--- core/src/time/drift_correction/ticks.rs
+++ core/src/time/drift_correction/ticks.rs
@@ -74,10 +74,10 @@ mod test {
   #[test]
   pub fn ticks_drift_correction_new() {
     let correction = TicksDriftCorrection::new(Signature::new(6, 13), Tempo::new(130), 44100);
-    assert_eq!(correction.ticks_per_sample, 4.190461073318216);
+    assert_eq!(correction.ticks_per_sample, 30719.999958427816);
     assert_eq!(
       correction.error_per_sample,
-      -0.000007558578987370399
+      -0.00000377929129568087
     );
     assert_eq!(correction.error_accumulated, 0.0);
     assert_eq!(correction.last_correction, 0.0);
@@ -87,14 +87,14 @@ mod test {
   pub fn ticks_drift_correction_next() {
     let mut correction = TicksDriftCorrection::new(Signature::new(4, 4), Tempo::new(60), 44100);
     let ticks = correction.next(100);
-    assert_eq!(ticks, TicksTime::new(628));
+    assert_eq!(ticks, TicksTime::new(4608000));
     let ticks = correction.next(100);
-    assert_eq!(ticks, TicksTime::new(628));
+    assert_eq!(ticks, TicksTime::new(4608000));
     let ticks = correction.next(100);
-    assert_eq!(ticks, TicksTime::new(627));
+    assert_eq!(ticks, TicksTime::new(4608000));
     let ticks = correction.next(100);
-    assert_eq!(ticks, TicksTime::new(628));
+    assert_eq!(ticks, TicksTime::new(4608000));
     let ticks = correction.next(100);
-    assert_eq!(ticks, TicksTime::new(627));
+    assert_eq!(ticks, TicksTime::new(4608000));
   }
 }
